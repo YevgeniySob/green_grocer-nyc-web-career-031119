1675370c0cfa54afc456471b24c195fea78f97e8
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-28 19:42:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,6 +19,11 @@ def apply_coupons(cart, coupons)
         cart["#{name} W/COUPON"][:count] += 1
       else
         cart["#{name} W/COUPON"] = {:price => ele[:cost], :clearence => cart[name][:clearence], :count => 1}
+      end
+      cart[name][:count] -= 1
+    end
+  end
+  cart
     
 end
 
